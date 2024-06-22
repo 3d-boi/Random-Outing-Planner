586b5cb80d69e9d4b5f4f226e16a5cb2444a5709
Marwan Hany <marwanhany313@gmail.com>
2024-06-22 23:59:35 +0300

    Made a simple web app to reference to 🤙

---
+++ Flask Web App/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/voteyes')
+def voteyes():
+    print('someone voted Yes')
+    return render_template('vote.html')
+
+@app.route('/voteno')
+def voteno():
+    print('someone voted No')
+    return render_template('vote.html')
+
+@app.route('/suggest')
+def suggest():
+    return render_template('suggest.html')
+
+app.run(debug=True)
+
