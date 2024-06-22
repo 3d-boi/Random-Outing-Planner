dbbb5ff51b70f8e33844df067f43f1ea55de30ed
Marwan Hany <marwanhany313@gmail.com>
2024-06-22 07:18:22 +0300

    Added HTML template to be implemented 📑

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ current_date = datetime.datetime.now().date()
 current_time = datetime.datetime.now().time()
  
 def generate_random_plan():
+    # Defining some needed variables
     offset_date = current_date + datetime.timedelta(days=random.randint(1,3))
     plan_time = datetime.time(random.randint(17,20),00)
-    plan = plan_template.plan(offset_date,plan_time,"location","note")
-    send_plan(offset_date,plan_time,random.choice(locations),"Bring board-games or bikes!")
+    plan_location = random.choice(locations)
 
-def send_plan(date:datetime.date, time:datetime.time, location:str, note:str):
+    # creating the "plan" object
+    plan = plan_template.plan(offset_date,plan_time,plan_location,"note")
+
+    # Sending an email with teh generated plan
+    send_plan(plan)
+
+def send_plan(plan:plan_template.plan):
     # Load the local .env file to be used
     load_dotenv('.env')
 
@@ -30,14 +36,13 @@ def send_plan(date:datetime.date, time:datetime.time, location:str, note:str):
 
     # Create and start a SMTP server
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-
         # Login to the sender's email account
         server.login(sender_email, email_passkey)
 
         # Construct and format the message to be sent
-        formatted_date = date.strftime("%A %d %b")
-        formatted_time = time.strftime("%I:%M %p")
-        msg = f"Subject: Random hangout plan\n\nGreetings,\nYou are invited to a random hagout with the members of the society,\nThe hangout will take place on {formatted_date}, at {formatted_time}, at '{location}', We are excited to see you there!\n\nNote: -{note}-\n\n\n The Planning Division."
+        formatted_date = plan.date.strftime("%A %d %b")
+        formatted_time = plan.time.strftime("%I:%M %p")
+        msg = f"Subject: Random hangout plan\n\nGreetings,\nYou are invited to a random hagout with the members of the society,\nThe hangout will take place on {formatted_date}, at {formatted_time}, at '{plan.location}', We are excited to see you there!\n\nNote: -{plan.note}-\n\n\n The Secret Planning Department."
 
         # Send email
         server.sendmail(sender_email,recipient_emails,msg)
@@ -45,5 +50,4 @@ def send_plan(date:datetime.date, time:datetime.time, location:str, note:str):
 
     print("Mail sent!")
 
-#send_plan(datetime.datetime.now(), "The School", "bring your bikes, we will race!")
 generate_random_plan()
