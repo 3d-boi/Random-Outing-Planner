b0f962484f4b72bf199af3b5f65515ac3ae0a45b
Marwan Hany <marwanhany313@gmail.com>
2024-06-28 19:23:52 +0300

    The form now saves date into csv files 🧠

--- Flask Web App/app.py
+++ Flask Web App/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -19,9 +20,21 @@ def voteno():
 @app.route('/suggest', methods = ["GET","POST"])
 def suggest():
     if request.method == "POST":
+        # Receive all the info from the HTML form.
         location = request.form.get('location')
         date = request.form.get('date')
         time = request.form.get('time')
+        is_repeating = request.form.get('frequency')
+
+        #Check id the plan is repeating or not.
+        if eval(is_repeating):
+            with open('Flask Web App/repeating.csv','a') as csv_repeating:
+                csv_repeating.write(f'\n{location},{date},{time}')
+            csv_repeating.close()
+        else:
+            with open('Flask Web App/onetime.csv','a') as csv_onetime:
+                csv_onetime.write(f'\n{location},{date},{time}')
+            csv_onetime.close()
         return render_template('suggested.html')
     return render_template('suggest.html')
 
